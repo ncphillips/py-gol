7290eb2f02e483176cd7fd769c865e6808153d5c
ncphillips <ncphillips.19@gmail.com>
2015-12-12 19:16:26 -0400

    Forget about size. Emptiness is the way.

--- tests/test_world.py
+++ tests/test_world.py
@@ -6,4 +6,9 @@ class WorldTest(unittest.TestCase):
 
     def test_new_world_is_empty(self):
         world = World()
-        self.assertEqual(0, world.size())
+        self.assertTrue(world.empty())
+
+    def test_a_world_with_a_cell_is_not_empty(self):
+        world = World()
+        world.add_cell(0, 0)
+        self.assertFalse(world.empty())
