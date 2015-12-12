ff2fb152f548abb7d101b37998b34f892bd2dc86
Brent Mitton <bmitton@gatech.edu>
2015-12-12 19:39:20 -0400

    make tests pass

--- src/gol.py
+++ src/gol.py
@@ -1,14 +1,13 @@
 class World:
 
     def __init__(self):
-        self.cells = []
-        pass
+        self.cells = set()
 
     def size(self):
-        return len(self.cells) - 1
+        return len(self.cells)
 
     def empty(self):
         return len(self.cells) == 0
 
     def add_cell(self, x, y):
-        self.cells.append((x, y))
+        self.cells.add((x, y))
